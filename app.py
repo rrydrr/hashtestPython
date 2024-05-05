cf6619ee2e870f0092a0c88335a35cafac04da03
rrydrr <135146023+rrydrr@users.noreply.github.com>
2024-05-05 18:39:56 +0700

    still broken but added input loop, sha-256 convert loop, and testing loop

--- app.py
+++ app.py
@@ -14,32 +14,35 @@ def convert_to_sha256(string):
     sha256_hash = hashlib.sha256(string.encode()).hexdigest()
     return sha256_hash
 
-def store_in_array(string):
-    """
-    Stores the SHA256 hash of a string in an array.
-
-    Args:
-        string (str): The string to be hashed and stored.
-
-    Returns:
-        None
-    """
-    sha256_hash = convert_to_sha256(string)
-    hash_array.append(sha256_hash)
-
-# Create an empty array to store the SHA-256 hashes
+# Create an empty array to store the SHA-256 hashes and strings
 hash_array = []
+string_array = []
 
-# Example usage
-scanned = input()
-input_string = scanned
-store_in_array(input_string)
+# Inputting to an array of strings
+while True:
+    print("Input string ('end' will end the string input): ", end="")
+    scanned = input()
+    if scanned == "end":
+        break
+    string_array.append(scanned)
 
-print(hash_array)
+# Converting array of strings to SHA-256 hashes
 
-print("Try to verify using the same input")
+for i in range(string_array):
+    hash_array.append(convert_to_sha256(string_array[i]))
+    
+# Testing the stored SHA-256 using regular string
 
-if convert_to_sha256(scanned) == hash_array[0]:
-    print("verfied")
-else:
-    print("not verified")
+while True:
+    print("Input string to check if string exists in hash array ('end' will end this process): ", end="")
+    scanned = input()
+    if scanned != "end":
+        for i in range(hash_array):
+            if scanned == hash_array[i]:
+                print("String found!")
+                found = True
+    
+        if not found:
+            print("String not found")
+    else:
+        break
